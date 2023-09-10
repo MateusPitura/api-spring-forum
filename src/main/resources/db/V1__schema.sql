c4406828ae33112bff9dec11bc6f50e9489f0a2c
MateusPitura <mateuspitura@gmail.com>
2023-09-10 19:20:22 -0300

    feat(data): nova versão do schema do banco
    Criado tabela de usuários e adicionado usuário padrão

--- src/main/resources/db/V1__schema.sql
+++ src/main/resources/db/V1__schema.sql
@@ -3,13 +3,21 @@ CREATE DATABASE IF NOT EXISTS forum;
 USE forum;
 
 CREATE TABLE IF NOT EXISTS topicos(
-    id bigint not null auto_increment,
-    titulo varchar(255) not null,
-    mensagem text,
-    data_criacao timestamp not null,
-    status boolean not null default '1',
-    autor varchar(255) not null,
-    curso enum('Java', 'MySQL', 'React') not null,
-    primary key(id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    mensagem TEXT,
+    data_criacao TIMESTAMP NOT NULL,
+    status BOOLEAN NOT NULL default '1',
+    autor VARCHAR(255) NOT NULL,
+    curso ENUM('Java', 'MySQL', 'React') NOT NULL,
+    PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS users(
+    id BIGINT NOT NULL auto_increment,
+    login VARCHAR(255) NOT NULL unique,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO users VALUES (DEFAULT, 'admin', 'admin');
